f67cdb5b85dac6444bc1b9f2b83af86d0af4dbbd
toy0756428 <48682582+toy0756428@users.noreply.github.com>
2019-05-14 21:20:58 +0800

    0756428

--- testcase/TestDeposit.java
+++ testcase/TestDeposit.java
@@ -34,7 +34,7 @@ public class TestDeposit {
 	public void test1949To1989() {
 		// 從1949存錢到昭和64, 存198178
 		// 利率都是3%
-		double expected = (198178 * Math.pow(1 + 0.03, 40));
+		double expected = 198178 * Math.pow(1 + 0.03, 40));
 
 		when(userA.depositAmount()).thenReturn(198178);
 		when(userA.depositInterest(anyInt)).thenReturn(0.03);
